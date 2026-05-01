c0378a9850cf4c3ac749c148b90c10de95d2732b
gabrielalejandrorianovargas-eng <gabrielalejandrorianovargas@gmail.com>
2026-05-01 01:30:44 -0500

    Add files via upload
    in this code text we will see how use variables and how use an aritmetic operation only with some basic steps and how this can help us in our daily life

---
+++ apple.py
@@ -0,0 +1,10 @@
+
+boy_name1 = "juan"
+boy_name2 = "maria"
+boy_name3 = "adan"
+var1 = 3
+var2 = 5
+var3 = 6
+print(boy_name1, var1,boy_name2, var2,boy_name3, var3, sep=",")
+total_apple = var1 + var2 + var3 
+print("total apples is equal to:", total_apple)
